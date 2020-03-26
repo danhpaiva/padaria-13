2ab0ea4dfb28bf72e3e3920985aed1438cb81055
Daniel Paiva <danpaiva@live.com>
2020-03-26 11:23:57 -0300

    first commit

---
+++ src/pratica13exercicio01.js
@@ -0,0 +1,4 @@
+function email () {
+    var nome = document.getElementById("exampleFormControlInput1").value;
+        document.getElementById("resposta").innerHTML="Agradecemos seu contato " + nome + " !";
+}
